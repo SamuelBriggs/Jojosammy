205ad444684d323e8a566df1bd8eb6cf8ddf5161
the_zen <alayandezainab64@gmail.com>
2024-02-23 23:48:32 +0100

    Mobile responsiveness in progress

--- src/pages/Misc.jsx
+++ src/pages/Misc.jsx
@@ -15,12 +15,12 @@ const Misc = () => {
     <div className='mt-32 ml-40'>
         <Header /> 
 
-        <div className='flex flex-row mt-20 -ml-2'>
-        <div className="">
-          <ul>
+        <div className='flex flex-row custom:flex-col mt-20 custom:-ml-40 custom:justify-center'>
+        <div className="mb-2 custom:flex custom:flex-row">
+          <ul className="flex flex-col w-full h-full custom:h-12 custom:pt-2 custom:border custom:mr-7 custom:ml-7 custom:flex custom:flex-row custom:space-x-20 custom:justify-center">
             <li
               onClick={() => handleItemClick("About")}
-              className={`cursor-pointer mb-2 text-sm  ${
+              className={`cursor-pointer mb-2 text-md sm:text-md  ${
                 selectedItem === "About" ? "text-green-500" : "text-white"
               }`}
             >
@@ -33,7 +33,7 @@ const Misc = () => {
             </li>
             <li
               onClick={() => handleItemClick("Writing")}
-              className={`cursor-pointer mb-2 text-sm ${
+              className={`cursor-pointer mb-2 text-md sm:text-md ${
                 selectedItem === "Writing" ? "text-green-500" : "text-white"
               }`}
             >
@@ -46,7 +46,7 @@ const Misc = () => {
             </li>
             <li
               onClick={() => handleItemClick("Misc")}
-              className={`cursor-pointer text-sm  ${
+              className={`cursor-pointer text-md sm:text-md  ${
                 selectedItem === "Misc" ? "text-green-500" : "text-white"
               }`}
             >
--- src/pages/Writing.jsx
+++ src/pages/Writing.jsx
@@ -24,12 +24,12 @@ const Writing = () => {
     <div className='mt-32 ml-40'>
         <Header /> 
 
-        <div className='flex flex-row mt-20 -ml-2'>
-        <div className="">
-          <ul>
+        <div className='flex flex-row custom:flex-col mt-20 custom:-ml-40 custom:justify-center'>
+        <div className="mb-2 custom:mb-8 custom:flex custom:flex-row">
+          <ul className="flex flex-col w-full h-full custom:h-12 custom:pt-2 custom:border custom:mr-7 custom:ml-7 custom:flex custom:flex-row custom:space-x-20 custom:justify-center">
             <li
               onClick={() => handleItemClick("About")}
-              className={`cursor-pointer mb-2 text-sm  ${
+              className={`cursor-pointer mb-2 text-md sm:text-md ${
                 selectedItem === "About" ? "text-[#22c55e]" : "text-white"
               }`}
             >
@@ -42,7 +42,7 @@ const Writing = () => {
             </li>
             <li
               onClick={() => handleItemClick("Writing")}
-              className={`cursor-pointer mb-2 text-sm  ${
+              className={`cursor-pointer mb-2 text-md sm:text-md  ${
                 selectedItem === "Writing" ? "text-[#22c55e]" : "text-white"
               }`}
             >
@@ -55,7 +55,7 @@ const Writing = () => {
             </li>
             <li
               onClick={() => handleItemClick("Misc")}
-              className={`cursor-pointer text-sm  ${
+              className={`cursor-pointer text-md sm:text-md  ${
                 selectedItem === "Misc" ? "text-[#22c55e]" : "text-white"
               }`}
             >
