75bc03865ca6ea2c1893bc666c1a201fae790dad
the_zen <alayandezainab64@gmail.com>
2024-02-24 07:36:08 +0100

    Mobile responsiveness in progress

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,28 +1,3 @@
-// import React from "react";
-// import Logo from "../images/svg/Logo.svg";
-
-// const Layout = () => {
-
-//   return (
-//     <div className="custom:-ml-20 custom:justify-center flex flex-col md:flex-row md:items-center 
-//     sm:flex-row sm:items-center lg:flex-row lg:items-center xl:flex-row xl:items-center 
-//     2xl:flex-row 2xl:items-center custom2:flex custom2:flex-row custom2:justify-center
-//     custom3:border custom3:flex custom3:flex-col custom3:w-80 custom3:justify-center custom3:-ml-12">
-//       <div className="flex-shrink-0 custom2:-ml-28">
-//         <img className="" style={{width: "35px", height: "35px"}} src={Logo} alt="No image yet" />
-//       </div>
-//       <div className="ml-40 custom:ml-25 text-white text-2xl font-bold 
-//       name custom:-mt-9 custom2:ml-12">
-//         Samuel Alawode
-//       </div>
-//     </div>
-//   );
-
-//  };
-
-// export default Layout;
-
-
 import React from "react";
 import Logo from "../images/svg/Logo.svg";
 
@@ -31,10 +6,11 @@ const Layout = () => {
     <div className="custom:-ml-20 custom:justify-center flex flex-col md:flex-row md:items-center 
     sm:flex-row sm:items-center lg:flex-row lg:items-center xl:flex-row xl:items-center 
     2xl:flex-row 2xl:items-center custom2:flex custom2:flex-row custom2:justify-center
-    custom3:border custom3:flex custom3:flex-col custom3:w-48 custom3:justify-center custom3:-ml-12">
+    custom3:flex custom3:flex-col custom3:w-48 custom3:justify-center 
+    custom3:-ml-12 custom3:h-32">
       <div className="flex-shrink-0 custom2:-ml-28 custom3:flex-col custom3:justify-center">
-        <img className="custom3:justify-center" style={{ width: "35px", height: "35px" }} src={Logo} alt="No image yet" />
-        <div className="text-white text-2xl font-bold name custom:-mt-2 custom2:ml-12">
+        <img className="custom3:justify-center custom3:ml-20 " style={{ width: "35px", height: "35px" }} src={Logo} alt="No image yet" />
+        <div className="text-white text-2xl font-bold name custom:-mt-2 custom2:ml-12 custom3:mt-8">
           Samuel Alawode
         </div>
       </div>
@@ -43,3 +19,8 @@ const Layout = () => {
 };
 
 export default Layout;
+
+
+
+
+
