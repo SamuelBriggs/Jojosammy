a363032765518b32ab1625fbab0ded6b863804bb
the_zen <alayandezainab64@gmail.com>
2024-02-24 07:30:02 +0100

    Mobile responsiveness in progress

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -31,9 +31,9 @@ const Layout = () => {
     <div className="custom:-ml-20 custom:justify-center flex flex-col md:flex-row md:items-center 
     sm:flex-row sm:items-center lg:flex-row lg:items-center xl:flex-row xl:items-center 
     2xl:flex-row 2xl:items-center custom2:flex custom2:flex-row custom2:justify-center
-    custom3:border custom3:flex custom3:flex-col custom3:w-80 custom3:justify-center custom3:-ml-12">
-      <div className="flex-shrink-0 custom2:-ml-28 custom3:flex-col custom3:items-center">
-        <img className="" style={{ width: "35px", height: "35px" }} src={Logo} alt="No image yet" />
+    custom3:border custom3:flex custom3:flex-col custom3:w-48 custom3:justify-center custom3:-ml-12">
+      <div className="flex-shrink-0 custom2:-ml-28 custom3:flex-col custom3:justify-center">
+        <img className="custom3:justify-center" style={{ width: "35px", height: "35px" }} src={Logo} alt="No image yet" />
         <div className="text-white text-2xl font-bold name custom:-mt-2 custom2:ml-12">
           Samuel Alawode
         </div>
