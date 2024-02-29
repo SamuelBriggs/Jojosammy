2ba477dd31c8c46c2a3f25d3b5d208918d73b5fd
the_zen <alayandezainab64@gmail.com>
2024-02-29 16:38:00 +0100

    taking care of larger screens

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -20,7 +20,7 @@ const HomePage = () => {
       custom:-ml-40 custom:justify-center custom2:flex-col custom2:-ml-40 custom2:justify-center
       custom3:flex-col custom3:-ml-40 custom3:justify-center custom3:mt-10 
       custom2:mt-10">
-        <div className="mb-2 custom:flex custom:flex-row
+        <div className="sm:-ml-16 mb-2 custom:flex custom:flex-row
         custom2:flex custom2:flex-row custom3:flex custom3:flex-row">
           <ul className="flex flex-col w-full h-full custom:h-12 custom:pt-2 custom:border custom:mr-7 custom:ml-7 custom:flex 
           custom:flex-row custom:space-x-24 custom:justify-center
